41e3418a3e393d46e00616a6acd8a3aeaee1f127
Karl Knechtel <zahlman@proton.me>
2024-07-01 15:09:19 -0400

    Work in a temporary directory
    `cookiecutter` is now instructed to create the new project as a
    subdirectory of a new temporary directory. A `src` folder is created and
    the specified contents are moved there; then everything is cleaned up.
    
    A temporary directory is necessary in case the chosen project name
    conflicts with a folder supplied for the initial `src`.

--- src/cookiebaker/__init__.py
+++ src/cookiebaker/__init__.py
@@ -1,7 +1,7 @@
 from cookiecutter.main import cookiecutter
 from datetime import datetime
 from pathlib import Path
-import sys
+import shutil, sys, tempfile
 
 _default_cookiecutter = str(Path(__file__).parent / 'default_cookiecutter')
 assert ':' not in _default_cookiecutter, "please use a path without colons"
@@ -9,7 +9,7 @@ assert ':' not in _default_cookiecutter, "please use a path without colons"
 
 def create(name, *sources):
     now = datetime.now()
-    extra_context = {
+    context = {
         '__project_name': name,
         '__pypi_slug': name.lower().replace(' ', '_').replace('-', '_'),
         '__rtd_slug': name.lower().replace(' ', '-').replace('_', '-'),
@@ -18,7 +18,20 @@ def create(name, *sources):
         '__year': now.year,
         '__month': now.month
     }
-    cookiecutter(_default_cookiecutter, extra_context=extra_context)
+    tmp = tempfile.mkdtemp(dir='.')
+    cookiecutter(_default_cookiecutter, output_dir=tmp, extra_context=context)
+    tmp = Path(tmp)
+    # The temporary directory should only contain the single directory
+    # that was created by `cookiecutter`.
+    contents = tmp.iterdir()
+    created = next(contents)
+    src = created / 'src'
+    assert not list(contents)
+    src.mkdir()
+    for source in sources:
+        shutil.move(source, src / source)
+    shutil.move(created, '.')
+    tmp.rmdir()
 
 
 def cli():
